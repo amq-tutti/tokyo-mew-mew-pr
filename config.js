3bb8a25ab524080aecac5d8cf823ba9a636efe35
dvitjuks <d.vitjuks@gmail.com>
2025-03-24 18:26:24 +0200

    Change config files and song list for Tokyo Mew Mew values

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    localStoragePrefix: "party-rank-sorter",
-    title: "Party Rank Sorter",
-    description: "Party rank sorter for your custom list of songs."
+    localStoragePrefix: "tokyo-mew-mew",
+    title: "Tokyo Mew Mew Party Rank Sorter",
+    description: "Party rank sorter for all \"Tokyo Mew Mew\" anime songs",
 };
